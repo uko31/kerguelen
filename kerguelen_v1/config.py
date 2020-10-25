52007f27c247cc34cd4b1c8389ec9f319385d3be
uko31 <m.gregoriades@gmail.com>
2020-10-25 15:26:33 +0100

    kerguelen v1.1

--- kerguelen_v1/config.py
+++ kerguelen_v1/config.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 class DefaultConfig(object):
     DEBUG             = False
     TESTING           = False
     VERSION           = '1.1'
     SECRET_KEY        = '61384#!oqsdfnh66546azeoid'
     AUTHOR            = 'm.gregoriades@gmail.com'
-    WEATHER_API_KEY   = 'OTFhMjllYzhkZWViYzU4YzJhZWI3NWQ2OThlZDgwMzU='
+    #WEATHER_API_KEY   = os.environ.get('WEATHER_API_KEY')
     WEATHER_URL       = 'http://api.openweathermap.org/data/2.5/weather'
     WEATHER_LATITUDE  = '47.518292'
     WEATHER_LONGITUDE = '-2.310201'
